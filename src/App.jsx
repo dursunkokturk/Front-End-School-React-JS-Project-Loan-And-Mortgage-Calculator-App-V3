453d4c321e7a5c8ab04f5943b64af7c2686d8055
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-05-02 11:06:38 +0300

    src klasoru icindeki app.jsx component icindeki container div'i icinde main alanina eklenen input elementi islemleri tamamlandi, app.css dosyasinda css duzenlemesi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ export default function App() {
           <a href=""><h4>Hepsini Sil</h4></a>
         </header>
         <main className='main'>
-          <h4>Kredi Tutarı</h4>
-          <div className="input-box">
-            <span className="prefix">£</span>
-            <input type="text" defaultValue={100.000}/>
+          <div className="form-group">
+            <h4>Kredi Tutarı</h4>
+            <div className="input-box">
+              <span className="prefix">₺</span>
+              <input type="text" defaultValue={100.000} />
+            </div>
           </div>
         </main>
       </div>
