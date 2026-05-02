9ef2fa3c58e7291691a4672f90eb451840aa81ed
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-05-02 08:14:59 +0300

    src klasoru icindeki app.jsx component icindeki container div'i icinde header alani olusturuldu header alaninda baslik ve link eklendi, main alani olusturuldu, main alaninda baslik ve input elementi eklendi, main alanina eklenen input elementi icin islemler devam edecek, app.css dosyasinda container div'i icindeki header ve main div'leri icin css duzenlemesi yapildi

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ export default function App() {
 
   return (
     <>
-    <h2>Kredi ve İpotek Hesaplama</h2>
+      <div className="container">
+        <header className='header'>
+          <h2>Kredi ve İpotek Hesaplama</h2>
+          <a href=""><h4>Hepsini Sil</h4></a>
+        </header>
+        <main className='main'>
+          <h4>Kredi Tutarı</h4>
+          <div className="input-box">
+            <span className="prefix">£</span>
+            <input type="text" defaultValue={100.000}/>
+          </div>
+        </main>
+      </div>
     </>
   )
 }
